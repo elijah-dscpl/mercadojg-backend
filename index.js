d3afab11af98f0e167e412be0be37e58e8440d46
elijah-dscpl <edmb707win@gmail.com>
2025-11-29 23:03:23 +0100

    Backend listo para deploy

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import express from "express";
+import serverless from "serverless-http";
 import cors from "cors";
-import usersRouter from "./routes/users.js"; // ruta de tus rutas de usuarios
 
 const app = express();
-app.use(cors()); // permite peticiones desde cualquier dominio
-app.use(express.json()); // para parsear JSON
+app.use(cors());
+app.use(express.json());
 
-app.use("/api/users", usersRouter);
+// Datos de prueba
+let productos = [
+  { id: 1, titulo: "Producto 1", descripcion: "Demo", precio: 100, imagen_url: "" },
+  { id: 2, titulo: "Producto 2", descripcion: "Demo", precio: 200, imagen_url: "" }
+];
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Backend corriendo...");
+// Rutas
+app.get("/api/products", (req, res) => {
+  res.json(productos);
 });
+
+export default serverless(app);
